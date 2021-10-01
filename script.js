e76ff04bc8b38070d1e572220a2de6ee49f3317c
Nafis2003 <54066977+Nafis2003@users.noreply.github.com>
2021-10-02 00:59:22 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+var cards=document.querySelectorAll(".cards");
+
+var response= fetch ("https://github.com/Nafis2003/time-tracking-dashboard/blob/23a6f3c6a4f90414145f791600f4c1df2242d850/data.json");
+var data=response.json();
+
