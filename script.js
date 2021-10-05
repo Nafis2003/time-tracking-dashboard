13db8cb3c14230debb0b8da9332b5bfd9ee7bfea
Nafis2003 <54066977+Nafis2003@users.noreply.github.com>
2021-10-05 11:17:55 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const  cardInfo=document.querySelectorAll(".info");
 const btns=document.querySelectorAll("#timeframes span");
 var data=[];
 var frame="weekly";
-var value={"daily":"day","weekly":"week", "monthly":"month"};
+var value={"daily":{text:"day",btnIndex:0},"weekly":{text:"week",btnIndex:1}, "monthly":{text:"month",btnIndex:2}};
 
 const fetchData= async()=>{
 var response=await fetch ("https://nafis2003.github.io/time-tracking-dashboard/data.json");
@@ -10,13 +10,17 @@ data=await response.json();
 }
 
 const showData=()=>{
-     btns[Object.keys(value).indexOf(frame)]. style.color="#fff";
      let index=0;
+     let btnIndex=value[frame].btnIndex;
+btns[btnIndex].style.color="#fff";
+if (btnIndex>0){btns[btnIndex-1].style.color="var(--Pale-blue)";}
+else{btns[btnIndex+2].style.color="var(--Pale-blue)";}
+if (btnIndex<2){btns[btnIndex+1].style.color="var(--Pale-blue)";}
 				data.map((info)=>{
 							var {title,timeframes}=info;
 							var timeframe=timeframes[frame];
 							var text=`<h2>${title}</h2>
-				<p>${timeframe.current}hrs<span>Last ${value[frame]} - ${timeframe.previous}hrs</span></p>	`
+				<p>${timeframe.current}hrs</p><span>Last ${value[frame].text} - ${timeframe.previous}hrs</span>	`
 							cardInfo[index].innerHTML=text;
 							index++;
 				}			
@@ -26,6 +30,5 @@ const showData=()=>{
 fetchData().then(()=>{showData()},()=>alert ("SOMETHING WENT WRONG"));
 
 btns.forEach(btn=>{
-				btn.addEventListener("click",()=>{frame=btn.textContent.toLowerCase();showData();});
+btn.addEventListener("click",()=>{	frame=btn.textContent.toLowerCase();showData();});
 });
-
